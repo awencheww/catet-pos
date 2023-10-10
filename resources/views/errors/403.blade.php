e040e865976943a7ca364f40526f8513dd1a2afc
Cherwen Molina <cherwenm@gmail.com>
2023-10-10 18:29:42 +0800

    Add custom 403 error page

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -18,7 +18,7 @@
 <body class="antialiased">
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
       <div class="max-w-xl mx-auto sm:px-6 lg:px-8 text-center">
-          <div class="flex items-center pt-8 sm:justify-start sm:pt-0 text-center" style="justify-content: center;">
+          <div class="flex items-center pt-8 justify-center sm:pt-0">
               <div class="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
                   403                    </div>
 
@@ -27,7 +27,7 @@
           </div>
           <div class="ml-4 text-lg text-gray-500 uppercase tracking-wider">
             <p>You don't have right access to this page.</p>
-            <a style="color:blueviolet; text-decoration:underline;" href="{{route('home')}}">Back to Home</a>
+            <a style="color:blueviolet;" class="underline" href="{{route('home')}}">Back to Home Page</a>
           </div>
       </div>
   </div>
